07128198555b5da99e0a5c9d157230125d5315f8
Tejaswi Nadahalli <nadahalli@gmail.com>
2019-01-11 15:13:39 +0000

    Fixed sequence number bug

--- examples/lightning.py
+++ examples/lightning.py
@@ -25,7 +25,7 @@ def is_unilateral_closing(t):
     if not (hex(t.locktime).startswith('0x20')):
         return False
     for i in t.inputs:
-        if not (hex(i.sequence_number).startswith('0x20')
+        if not (hex(i.sequence_number).startswith('0x80')
                 and len(i.script.hex) == 0 and len(i.witnesses) > 0):
             return False
     return True
@@ -39,7 +39,7 @@ for block in blockchain.get_ordered_blocks(
         sys.argv[1] + '/index',
         cache='./cache.txt',
         start=start_block,
-        end=start_block + 30):
+        end=start_block + 10):
     print("height=%d, block=%s, block timestamp=%s" %
           (block.height, block.hash, str(block.header.timestamp)))
     for transaction in block.transactions:
